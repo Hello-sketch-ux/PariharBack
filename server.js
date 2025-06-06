4d89aedbc2f22c9507f116a5910a1e3cd77cec14
MohitMorya2 <mohitmorya56@gmail.com>
2025-06-06 21:03:07 +0530

    fix: add missing newline for improved readability in CORS configuration

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
