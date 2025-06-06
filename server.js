4f1478f64967749a2791667f1bcc4d4d94c8ae13
MohitMorya2 <mohitmorya56@gmail.com>
2025-06-06 20:52:08 +0530

    enhance CORS configuration to include allowed methods and headers

--- server.js
+++ server.js
@@ -30,6 +30,19 @@ app.use(cors({
     }
   },
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.options('*', cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS Not Allowed'));
+    }
+  },
+  credentials: true,
 }));
 app.use(express.json());
 app.use(cookieParser());
